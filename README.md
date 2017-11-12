a3e4fc119ba982b2f26fd6c37402b34573cb2d54
Jiaren Liu <jrliu@alauda.io>
2017-11-12 18:02:23 +0800

    add more proposals

--- README.md
+++ README.md
@@ -9,7 +9,17 @@ Yet another Babel preset for TypeScript, only transforms proposals which TypeScr
 
 So that you can use babel to transform proposals which are current in stage 0-2 and TypeScript team [will not implement them temporarily](https://github.com/Microsoft/TypeScript/issues/19044#event-1293164503).
 
-For now only [babel-plugin-transform-do-expressions](https://www.npmjs.com/package/babel-plugin-transform-do-expressions) and [babel-plugin-transform-function-bind](https://www.npmjs.com/package/babel-plugin-transform-function-bind) are included.
+## Enabled proposal plugins
+
+1. [do-expressions](https://www.npmjs.com/package/@babel/plugin-proposal-do-expressions)
+2. [function-bind](https://www.npmjs.com/package/@babel/plugin-proposal-function-bind)
+3. [function-sent](https://www.npmjs.com/package/@babel/plugin-proposal-function-sent)
+4. [nullish-coalescing-operator](https://www.npmjs.com/package/@babel/plugin-proposal-nullish-coalescing-operator)
+5. [numeric-separator](https://www.npmjs.com/package/@babel/plugin-proposal-numeric-separator)
+6. [optional-chaining](https://www.npmjs.com/package/@babel/plugin-proposal-optional-chaining)
+7. [pipeline-operator](https://www.npmjs.com/package/@babel/plugin-proposal-pipeline-operator)
+8. [throw-expressions](https://www.npmjs.com/package/@babel/plugin-proposal-throw-expressions)
+9. [unicode-property-regex](https://www.npmjs.com/package/@babel/plugin-proposal-unicode-property-regex)
 
 ## Install
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import syntaxDecorators from '@babel/plugin-syntax-decorators'
 import syntaxDynamicImport from '@babel/plugin-syntax-dynamic-import'
 import syntaxTypeScript from '@babel/plugin-syntax-typescript'
-import transformDoExpressions from 'babel-plugin-transform-do-expressions'
-import transformFunctionBind from 'babel-plugin-transform-function-bind'
+
+import proposalDoExpressions from '@babel/plugin-proposal-do-expressions'
+import proposalFunctionBind from '@babel/plugin-proposal-function-bind'
+import proposalFunctionSent from '@babel/plugin-proposal-function-sent'
+import proposalNullishCoalescingOperator from '@babel/plugin-proposal-nullish-coalescing-operator'
+import proposalNumericSeparator from '@babel/plugin-proposal-numeric-separator'
+import proposalOptionalChaining from '@babel/plugin-proposal-optional-chaining'
+import proposalPipelineOperator from '@babel/plugin-proposal-pipeline-operator'
+import proposalThrowExpression from '@babel/plugin-proposal-throw-expressions'
+import proposalUnicodePropertyRegex from '@babel/plugin-proposal-unicode-property-regex'
 
 export default () => ({
   plugins: [
     syntaxDecorators,
     syntaxDynamicImport,
     syntaxTypeScript,
-    transformDoExpressions,
-    transformFunctionBind
+    proposalDoExpressions,
+    proposalFunctionBind,
+    proposalFunctionSent,
+    proposalNullishCoalescingOperator,
+    proposalNumericSeparator,
+    proposalOptionalChaining,
+    proposalPipelineOperator,
+    proposalThrowExpression,
+    proposalUnicodePropertyRegex
   ]
 })
