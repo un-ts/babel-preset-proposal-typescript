aceb6dfcbebbed6aa02398b542054c708857c452
JounQin <admin@1stg.me>
2021-06-04 17:09:34 +0800

    fix: lint error

--- src/shim.d.ts
+++ src/shim.d.ts
@@ -1,7 +1,7 @@
 declare module '@babel/helper-plugin-utils' {
   import { ConfigAPI, PluginItem } from '@babel/core'
+
   export const declare: (
-    // eslint-disable-next-line @typescript-eslint/ban-types
     fn: (api: ConfigAPI, options: object) => PluginItem,
   ) => PluginItem
 }
