1896a9f74af2194072a21ae0a14a28dfce51aca3
JounQin <admin@1stg.me>
2021-06-17 12:34:53 +0800

    chore: bump all (dev)Dependencies, remove unnecessary deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,7 @@ const {
 const tsBase = ts[0]
 
 module.exports = {
-  extends: ['@1stg/eslint-config/base', 'plugin:import/typescript'],
-  settings: {
-    'import/ignore': [/test\/private-methods.ts$/],
-    'import/parsers': {
-      '@typescript-eslint/parser': [],
-      '@babel/eslint-parser': ['.ts'],
-    },
-    ...tsBase.settings,
-  },
+  extends: ['@1stg/eslint-config/base'],
   overrides: [
     tsBase,
     {
